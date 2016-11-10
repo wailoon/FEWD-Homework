a60475b842b3e092de85c5982e1c597bb8eada7a
wailoon <wailoon@WAILOONs-MBP.home>
2016-11-09 23:50:04 -0500

    final project update

--- FINAL PROJECT/waru/js/waru.js
+++ FINAL PROJECT/waru/js/waru.js
@@ -2,102 +2,123 @@ $(document).ready(function(){
 
    console.log("here"); //log to show that it works so far
 
-function signs() {
-var start = 1901, birthyear = document.zodiac.year.value, date=document.zodiac.date.value, month=document.zodiac.month.selectedIndex;
+var year; //declaring city input as a variable
 
-// with (document.zodiac.sign){
+	$("#find-button").on("click", function(e){ //creating an event function when update button is clicked
+		e.preventDefault();
+		year = $("#birth-year").val(); //recording the city input by user
+		console.log(year);  //log to show that it works so far
+		$("#birth-year").val(""); //Reset the user input field after it is submitted
 
-// if (month == 1 && date >=20 || month == 2 && date <=18) {value = "Aquarius";}
-// if (month == 1 && date > 31) {value = "Huh?";}
-// if (month == 2 && date >=19 || month == 3 && date <=20) {value = "Pisces";}
-// if (month == 2 && date > 29) {value = "Say what?";}
-// if (month == 3 && date >=21 || month == 4 && date <=19) {value = "Aries";}
-// if (month == 3 && date > 31) {value = "OK.  Whatever.";}
-// if (month == 4 && date >=20 || month == 5 && date <=20) {value = "Taurus";}
-// if (month == 4 && date > 30) {value = "I'm soooo sorry!";}
-// if (month == 5 && date >=21 || month == 6 && date <=21) {value = "Gemini";}
-// if (month == 5 && date > 31) {value = "Umm ... no.";}
-// if (month == 6 && date >=22 || month == 7 && date <=22) {value = "Cancer";}
-// if (month == 6 && date > 30) {value = "Sorry.";}
-// if (month == 7 && date >=23 || month == 8 && date <=22) {value = "Leo";}
-// if (month == 7 && date > 31) {value = "Excuse me?";}
-// if (month == 8 && date >=23 || month == 9 && date <=22) {value = "Virgo";}
-// if (month == 8 && date > 31) {value = "Yeah. Right.";}
-// if (month == 9 && date >=23 || month == 10 && date <=22) {value = "Libra";}
-// if (month == 9 && date > 30) {value = "Try Again.";}
-// if (month == 10 && date >=23 || month == 11 && date <=21) {value = "Scorpio";}
-// if (month == 10 && date > 31) {value = "Forget it!";}
-// if (month == 11 && date >=22 || month == 12 && date <=21) {value = "Sagittarius";}
-// if (month == 11 && date > 30) {value = "Invalid Date";}
-// if (month == 12 && date >=22 || month == 1 && date <=19) {value = "Capricorn";}
-// if (month == 12 && date > 31) {value = "No way!";}
-// }
+		//changing out photos per user input
+		if(year === "1912" || year === "1924" || year === "1936" || year === "1948" || year === "1960" || year === "1972" || year === "1984" || year === "1996" || year === "2008" || year === "2020") { 
+			$(".rat").slideToggle( "medium" ); 
+    	// Animation complete.
+    		$("this").addClass("rat");
+ 			$("section").removeClass("ox tiger rabbit dragon snake horse goat monkey rooster dog pig"); 
+	   		$("div").removeClass("cover");
+			$("#birth-year").val(""); //Reset the user input field after it is submitted to the value given
+		}
 
-x = (start - birthyear) % 12
-with (document.zodiac.sign){
-if (x == 1 || x == -11) {value = "Rat";}
-if (x == 0) {value = "Ox";}
-if (x == 11 || x == -1) {value = "Tiger";}
-if (x == 10 || x == -2) {value = "Rabbit";}
-if (x == 9 || x == -3)  {value = "Dragon";}
-if (x == 8 || x == -4)  {value ="Snake";}
-if (x == 7 || x == -5)  {value = "Horse";}
-if (x == 6 || x == -6)  {value = "Sheep";}
-if (x == 5 || x == -7)  {value = "Monkey";}
-if (x == 4 || x == -8)  {value = "Rooster";}
-if (x == 3 || x == -9)  {value = "Dog";}
-if (x == 2 || x == -10)  {value = "Pig";}  
-}
-}
+		else if(year === "1913" || year === "1925" || year === "1937" || year === "1949" || year === "1961" || year === "1973" || year === "1985" || year === "1997" || year === "2009" || year === "2021") { 
+  			$(".ox").slideToggle( "medium" ); 
+    	// Animation complete.
+	  		$("section").addClass("ox");
+	  		$("div").removeClass("cover");
+  			$("section").removeClass("rat tiger rabbit dragon snake horse goat monkey rooster dog pig");  
+			$("#birth-year").val(""); //Reset the user input field after it is submitted to the value given
+		}
+
+		else if(year === "1914" || year === "1926" || year === "1938" || year === "1950" || year === "1962" || year === "1974" || year === "1986" || year === "1998" || year === "2010" || year === "2022") { 
+			$(".tiger").slideToggle( "medium" ); 
+    	// Animation complete.
+    	    $("section").addClass("tiger");
+    		$("div").removeClass("cover");
+			$("section").removeClass("ox rat rabbit dragon snake horse goat monkey rooster dog pig"); 
+    		// $(".cover").slideToggle( "fast" ); 
+			$("#birth-year").val(""); //Reset the user input field after it is submitted to the value given
+		}
+
+		else if(year === "1915" || year === "1927" || year === "1939" || year === "1951" || year === "1963" || year === "1975" || year === "1987" || year === "1999" || year === "2011" || year === "2023") { 
+			$(".rabbit").slideToggle( "medium" ); 
+			$("section").addClass("rabbit");
+    	// Animation complete.
+    		$("div").removeClass("cover");
+			$("section").removeClass("ox tiger rat dragon snake horse goat monkey rooster dog pig"); 			
+			$("#birth-year").val(""); //Reset the user input field after it is submitted to the value given
+		}
+
+		else if(year === "1916" || year === "1928" || year === "1940" || year === "1952" || year === "1964" || year === "1976" || year === "1988" || year === "2000" || year === "2012" || year === "2024") { 
+			$(".dragon").slideToggle( "medium" ); 
+			$("section").addClass("dragon");
+    	// Animation complete.
+    		$("div").removeClass("cover");
+			$("section").removeClass("ox tiger rabbit rat snake horse goat monkey rooster dog pig"); 
+			$("#birth-year").val(""); //Reset the user input field after it is submitted to the value given
+		}
+
+		else if(year === "1917" || year === "1929" || year === "1941" || year === "1953" || year === "1965" || year === "1977" || year === "1989" || year === "2001" || year === "2013" || year === "2025") { 
+ 			$(".snake").slideToggle( "medium" ); 
+	   		$("section").addClass("snake");
+    	// Animation complete.
+    		$("div").removeClass("cover");
+			$("section").removeClass("ox tiger rabbit dragon rat horse goat monkey rooster dog pig"); 
+			$("#birth-year").val(""); //Reset the user input field after it is submitted to the value given
+		}
+
+		else if(year === "1918" || year === "1930" || year === "1942" || year === "1954" || year === "1966" || year === "1978" || year === "1990" || year === "2002" || year === "2014" || year === "2026") { 
+			$(".horse").slideToggle( "medium" ); 
+    		$("section").addClass("horse");
+    	// Animation complete.
+    		$("div").removeClass("cover");
+			$("section").removeClass("ox tiger rabbit dragon snake rat goat monkey rooster dog pig"); 
+			$("#birth-year").val(""); //Reset the user input field after it is submitted to the value given
+		}
+
+		else if(year === "1919" || year === "1931" || year === "1943" || year === "1955" || year === "1967" || year === "1979" || year === "1991" || year === "2003" || year === "2015" || year === "2027") { 
+ 			$(".goat").slideToggle( "medium" ); 
+	   		$("section").addClass("goat");
+    	// Animation complete.
+    		$("div").removeClass("cover");
+			$("section").removeClass("ox tiger rabbit dragon snake horse rat monkey rooster dog pig");  
+			$("#birth-year").val(""); //Reset the user input field after it is submitted to the value given
+		}
+
+		else if(year === "1920" || year === "1932" || year === "1944" || year === "1956" || year === "1968" || year === "1980" || year === "1992" || year === "2004" || year === "2016" || year === "2028") { 
+			$(".monkey").slideToggle( "medium" ); 
+    		$("section").addClass("monkey");
+    	// Animation complete.
+    		$("div").removeClass("cover");
+			$("section").removeClass("ox tiger rabbit dragon snake horse goat rat rooster dog pig");  
+			$("#birth-year").val(""); //Reset the user input field after it is submitted to the value given
+		}
+
+		else if(year === "1921" || year === "1933" || year === "1945" || year === "1957" || year === "1969" || year === "1981" || year === "1993" || year === "2005" || year === "2017" || year === "2029") { 
+			$(".rooster").slideToggle( "medium" ); 
+    		$("section").addClass("rooster");
+    	// Animation complete.
+    		$("div").removeClass("cover");
+			$("section").removeClass("ox tiger rabbit dragon snake horse goat monkey rat dog pig"); 
+			$("#birth-year").val(""); //Reset the user input field after it is submitted to the value given
+		}
+
+		else if(year === "1922" || year === "1934" || year === "1946" || year === "1958" || year === "1970" || year === "1982" || year === "1994" || year === "2006" || year === "2018" || year === "2030") { 
+			$(".dog").slideToggle( "medium" ); 
+    		$("section").addClass("dog");
+    	// Animation complete.
+    		$("div").removeClass("cover");
+			$("section").removeClass("ox tiger rabbit dragon snake horse goat monkey rooster rat pig");  
+			$("#birth-year").val(""); //Reset the user input field after it is submitted to the value given
+		}
+
+		else if(year === "1923" || year === "1935" || year === "1947" || year === "1959" || year === "1971" || year === "1983" || year === "1995" || year === "2007" || year === "2019" || year === "2031") { 
+			$(".pig").slideToggle( "medium" ); 
+    		$("section").addClass("pig");
+    	// Animation complete.
+    		$("div").removeClass("cover");
+			$("section").removeClass("ox tiger rabbit dragon snake horse goat monkey rooster dog rat"); 
+			$("#birth-year").val(""); //Reset the user input field after it is submitted to the value given
+		}
+	});
+
 });
-//  End -->
-
-
- //   // $("#city-type").on("click", function(e) {
- //   // 		e.preventDefault();
- //   // 		console.log("city type clicked")
-
- //   //Add "NYC", "SF", "LA", "ATX", "SYD" to the dropdown menu selection
- //   		var arrayCity = ["NYC", "SF", "LA", "ATX", "SYD"];
-      
- //      //Creating a for loop by calling arrayCity
- //      for (var i = 0; i <= 4; i++) {
- //       $("#city-type").append($("<option>"+arrayCity[i]+"</option>"))
- //      };
-
-	// $("#city-type").change(function() { //creating an event function when update button is clicked
-	// 	var city = $("#city-type").val(); //recording the city input by user
-	// 	console.log(city);  //log to show that it works so far
-	// 	$("#city-type").val(""); //Reset the user input field after it is submitted
-
-	// 	//changing out photos per user input
-	// 	if(city === "NYC" || city === "NY" || city === "New York" || city === "New York City" || 
-	// 		city === "nyc" || city === "ny" || city === "new york" || city === "new york city") {
-	// 		$("body").addClass("nyc");
-	// 		$("body").removeClass("sf la austin sydney");
-	// 		$("#city-type").val("Too busy!"); //Reset the user input field after it is submitted to the value given
-	// 	}
-	// 	else if(city === "SF"|| city === "San Francisco" || city === "san francisco" || city === "sf") {
-	// 		$("body").addClass("sf");
-	// 		$("body").removeClass("nyc la austin sydney");
-	// 		$("#city-type").val("Such a beautiful city!"); //Reset the user input field after it is submitted to the value given
-	// 	}
-	// 	else if(city === "LA" || city === "Los Angeles" || city === "la" || city === "lax" || city === "LAX") {
-	// 		$("body").addClass("la");
-	// 		$("body").removeClass("sf nyc austin sydney");
-	// 		$("#city-type").val("For celebrities only!"); //Reset the user input field after it is submitted to the value given
-	// 	}
-	// 	else if(city === "ATX" || city === "Austin" || city === "atx" || city === "austin") {
-	// 		$("body").addClass("austin");
-	// 		$("body").removeClass("sf la nyc sydney");
-	// 		$("#city-type").val("Too hot for me!"); //Reset the user input field after it is submitted to the value given
-	// 	}
-	// 	else if(city === "SYD" || city === "Sydney" || city === "syd" || city === "sydney") {
-	// 		$("body").addClass("sydney");
-	// 		$("body").removeClass("sf la austin nyc");
-	// 		$("#city-type").val("Must visit!"); //Reset the user input field after it is submitted to the value given
-	// 	}
-
-// })
-
-// });
